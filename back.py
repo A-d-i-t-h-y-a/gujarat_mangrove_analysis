55d636c543d894272c39a8d96ba043bb032b30ad
Adithya <periketi.adithyachary@gmail.com>
2023-06-22 20:52:18 +0530

    latest commit

--- back.py
+++ back.py
@@ -2,14 +2,33 @@ from flask import Flask, render_template, request, jsonify
 import datacube
 import io
 import odc.algo
+import matplotlib
+matplotlib.use('Agg')  # Use non-GUI backend
 import matplotlib.pyplot as plt
 from datacube.utils.cog import write_cog
 import base64
+import numpy as np
 
 from deafrica_tools.plotting import display_map, rgb
 dc = datacube.Datacube(app="04_Plotting")
 # 15.85828652, 80.78694696
 # 15.75418332, 81.02203692
+
+from geopy.geocoders import Nominatim
+
+def get_area_name(latitude, longitude):
+    geolocator = Nominatim(user_agent='my-app')  # Initialize the geocoder
+    location = geolocator.reverse((latitude, longitude))  # Reverse geocode the coordinates
+    if location is not None:
+        address_components = location.raw['address']
+        city_name = address_components.get('city', '')
+        if not city_name:
+            city_name = address_components.get('town', '')
+        if not city_name:
+            city_name = address_components.get('village', '')
+        return city_name
+    else:
+        return "City name not found"
 
 app = Flask(__name__)
 
@@ -24,31 +43,63 @@ def my_flask_function():
         lmax = request.json['lat_max']
         lnmin = request.json['lng_min']
         lnmax = request.json['lng_max']
+        td = request.json['todate']
+        fd = request.json['fromdate']
+        index = request.json['index']
 
         lat_range = (lmin, lmax)
         lon_range = (lnmin, lnmax)
         print(lat_range, lon_range)
-        time_range = ('2022-01-15', '2023-02-15')
+        if(td=="" or fd==""):
+            query = {
+                "measurements":["B04_10m","B03_10m","B02_10m", "B08_10m", "SCL_20m"],
+                "x":lon_range,
+                "y":lat_range,
+                "output_crs":'EPSG:6933',
+                "resolution":(-30, 30)
+            }
+        else:
+            query = {
+                "measurements":["B04_10m","B03_10m","B02_10m", "B08_10m", "SCL_20m"],
+                "x":lon_range,
+                "y":lat_range,
+                "time": (fd, td),
+                "output_crs":'EPSG:6933',
+                "resolution":(-30, 30)
+            }
         # display_map(x=lon_range, y=lat_range)
         try:
-            ds = dc.load(product="s2a_sen2cor_granule",
-                            measurements=["B04_10m","B03_10m","B02_10m", "B08_10m", "SCL_20m"],
-                        x=lon_range,
-                        y=lat_range,
-                        time=time_range,
-                        output_crs='EPSG:6933',
-                        resolution=(-30, 30))
+            ds = dc.load(product="s2a_sen2cor_granule",**query)
             dataset = ds
             dataset =  odc.algo.to_f32(dataset)
-            band_diff = dataset.B08_10m - dataset.B04_10m
-            band_sum = dataset.B08_10m + dataset.B04_10m
+            if(index == 'NDVI'):
+                band_diff = dataset.B08_10m - dataset.B04_10m;
+                band_sum = dataset.B08_10m + dataset.B04_10m;
+                index = band_diff/band_sum;
+            else:
+                band_diff = dataset.B03_10m - dataset.B08_10m
+                band_sum = dataset.B03_10m + dataset.B08_10m
+
+                # Calculate NDVI and store it as a measurement in the original dataset
+                index = band_diff / band_sum
+
         except Exception as e:
             return jsonify({'error': "No Data Found"})
 
         # Calculate NDVI and store it as a measurement in the original dataset
-        ndvi = band_diff / band_sum
+        labels = list(map(lambda x: x.split('T')[0], [i for i in np.datetime_as_string(index.time.values).tolist()]))
+
+
+        masked_ds_mean = index.mean(dim=['x', 'y'], skipna=True)
+        # mean_ndvi = selected_times.mean(dim='time')  # Calculate the mean along the 'time' dimension
+
+        # Print the resulting mean_ndvi
+        area_name = get_area_name(np.mean(lat_range), np.mean(lon_range))
+                # print(area_name)
+        data = list(map(lambda x:round(x, 4), masked_ds_mean.values.tolist()))
         plt.figure(figsize=(8, 8))
-        ndvi.plot(col='time', vmin=0, vmax=1, col_wrap=3)
+        subset = index.isel(time=[0, -1])
+        subset.plot(col='time', vmin=0, vmax=1, col_wrap=3)
         img_buffer = io.BytesIO()
         plt.savefig(img_buffer, format='png')
         img_buffer.seek(0)
@@ -57,7 +108,29 @@ def my_flask_function():
         img_base64 = base64.b64encode(img_buffer.getvalue()).decode()
 
     # Return the base64 encoded PNG image as JSON
-        return jsonify({'image': img_base64})
+        return jsonify({'image': img_base64, 'labels': labels, 'data': data, 'area': area_name})
     # Calculate the components that make up the NDVI calculation
+
+# @app.route('/update-graph', methods=['POST'])
+# def update_graph():
+#     # Process the request and fetch updated data
+#     # Generate the updated graph using the new data
+#     # Save the updated graph as an image file (similar to the previous example)
+#     x = [1, 2, 3, 4, 5]
+#     y = [10, 20, 30, 25, 35]
+#     plt.plot(x, y)
+#     plt.xlabel('X-axis')
+#     plt.ylabel('Y-axis')
+
+#     # Save the graph as an image file
+#     image = io.BytesIO()
+#     plt.savefig(image, format='png')
+#     image.seek(0)
+
+#     # Encode the image as base64 string
+#     graph_url = base64.b64encode(image.getvalue()).decode()
+
+#     # Return the URL or data of the updated graph
+#     return jsonify(graph_url=graph_url)
 
 app.run(debug=True)
--- static/temp.js
+++ static/temp.js
@@ -1,14 +1,168 @@
-var map = L.map('map').setView([15.805, 80.9], 10);
+let fd = document.getElementsByName("fromdate")[0];
+let td = document.getElementsByName("todate")[0];
+let today = new Date().toISOString().split("T")[0];
+td.max = today;
+fd.max = today;
+// const initialData = {
+//   labels: [],
+//   datasets: [],
+// };
+
+// // Create a Chart.js line chart
+// const ctx = document.getElementById('chartCanvas').getContext('2d');
+// const chart = new Chart(ctx, {
+//   type: 'line',
+//   data: initialData,
+// });
+
+// // Function to append new data to the chart
+// function appendData(x, y, color) {
+//   // Check if a dataset already exists
+//   if (chart.data.datasets.length === 0) {
+//     // Create a new dataset
+//     chart.data.datasets.push({
+//       label: 'Line Plot',
+//       data: [],
+//       borderColor: color,
+//       fill: false,
+//     });
+//   }
+
+//   // Get the first dataset
+//   const dataset = chart.data.datasets[0];
+
+//   // Add the new data point
+//   dataset.data.push({ x, y });
+
+//   // Update the chart
+//   chart.update();
+// }
+
+// let labels = [];
+
+let labels;
+
+// const chartOptions = {
+//   responsive: true,
+//   interaction: {
+//     intersect: false,
+//     mode: 'index'
+//   },
+//   scales: {
+//     x: {
+//       display: true
+//     },
+//     y: {
+//       display: true,
+//       suggestedMin: 0,
+//       suggestedMax: 100
+//     }
+//   }
+// };
+let chart, data;
+
+// data = {
+//   labels: labels,
+//   datasets: []
+// };
+// let ctv = document.getElementById('chartCanvas');
+// Create the chart
+// chart = new Chart(ctv, {
+//   type: 'line',
+//   data: data,
+//   options: chartOptions
+// });
+let map = L.map('map').setView([15.805, 80.9], 10);
+
+let taskExecuted = false;
+
+
+function performTask(lab) {
+  if (!taskExecuted) {
+    // Task logic goes here
+    labels = lab;
+    const ctx = document.getElementById('chartCanvas');
+    let mgr = document.getElementById('mgr');
+    let head = document.getElementById('thead');
+    var newRow = document.createElement('tr');
+
+    // Create the HTML content for the new row
+    var rowContent = `<th scope="row">#</th>`;
+    for(i of lab){
+      rowContent+=`<th>${i}</th>`;
+    }
+    newRow.innerHTML = rowContent;
+    head.appendChild(newRow)
+
+    // Set the HTML content of the new row
+    newRow.innerHTML = rowContent;
+    mgr.style.display = "block"
+    map.invalidateSize();
+
+    const chartOptions = {
+      responsive: true,
+      interaction: {
+        intersect: false,
+        mode: 'index'
+      },
+      scales: {
+        x: {
+          display: true
+        }
+        // y: {
+        //   display: true,
+        //   suggestedMin: -1,
+        //   suggestedMax: 1
+        // }
+      }
+    };
+
+    data = {
+      labels: labels,
+      datasets: []
+    };
+
+    // Create the chart
+    chart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: chartOptions
+    });
+
+    // Set the flag to true to indicate that the task has been executed
+    taskExecuted = true;
+  }
+}
+
+// performTask([])
+
+function appendData(newData) {
+  const dataset = chart.data.datasets;
+  // print(type(dataset))
+  dataset.push(newData);
+  chart.update();
+}
+
+// setTimeout(() => {
+//     appendData([70, 61, 85, 83, 58, 59, 40]);
+//   }, 1000);
+
+// console.log(td.max)
+// function handleOnChange(){
+
+//   // console.log(todate, fromdate)
+// }
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 // L.marker([51.5, -0.09]).addTo(map)
 // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
 // .openPopup();
 // var marker = L.marker([15.7, 80.85]).addTo(map);
-var latlng = L.latLng(15.7, 80.85);
+let latlng = L.latLng(15.7, 80.85);
 // var polygon = L.polygon([
 // [51.509, -0.08],
 // [55.503, -0.08],
@@ -19,11 +173,11 @@ var latlng = L.latLng(15.7, 80.85);
 // // circle.bindPopup("I am a circle.");
 // polygon.bindPopup("I am a polygon.");
 // var popup = L.popup()
-    // .setLatLng([15.713, 80.95])
-    // .setContent("You can start selecting from here")
-    // .openOn(map);
-    // var tooltip = L.tooltip(latlng, {content: 'Hello world!<br />This is a nice tooltip.'})
-    // .addTo(map);
+// .setLatLng([15.713, 80.95])
+// .setContent("You can start selecting from here")
+// .openOn(map);
+// var tooltip = L.tooltip(latlng, {content: 'Hello world!<br />This is a nice tooltip.'})
+// .addTo(map);
 // var corner1 = L.latLng(15.712, 80.927),
 // corner2 = L.latLng(15.774, 81.125),
 // bounds = L.latLngBounds(corner1, corner2);
@@ -36,14 +190,14 @@ var latlng = L.latLng(15.7, 80.85);
 //         .setContent("You clicked the map at " + e.latlng.toString())
 //         .openOn(map);
 // }
-var drawnItems = L.featureGroup().addTo(map);
+let drawnItems = L.featureGroup().addTo(map);
 
 // create a Rectangle draw handler
-var drawControl = new L.Control.Draw({
+let drawControl = new L.Control.Draw({
   draw: {
     rectangle: {
       shapeOptions: {
-        color: '#ffcc33',
+        color: "black",
         weight: 3
       }
     },
@@ -58,32 +212,89 @@ var drawControl = new L.Control.Draw({
   }
 }).addTo(map);
 
-function handleOnChange(e){
-  console.log(e.target.value)
+// Example using the Fetch API
+// function updateGraph() {
+//   fetch('/update-graph', {
+//     method: 'POST',
+//     // Include any necessary data or parameters for the action
+//     // body: JSON.stringify({ key: value })
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       // Handle the response data
+//       // Update the graph using the new data or URL
+//       // For example, update the 'src' attribute of the <img> tag
+//       const im = document.createElement('img');
+//       const img = document.getElementById('graph-img');
+//       im.src = 'data:image/png;base64,' + data.graph_url;
+//       img.appendChild(im);
+//     })
+//     .catch(error => {
+//       // Handle any error that occurred during the request
+//       console.error('Error:', error);
+//     });
+// }
+
+
+function getRandomColor() {
+  // Generate a random color in hexadecimal format
+  // Generate random RGB values in the range of 128-255 (instead of 0-128)
+  // Generate random RGB values in the range of 0-255
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+
+  // Convert the RGB components to hexadecimal and concatenate them
+  return "#" + ((1 << 24) | (red << 16) | (green << 8) | blue).toString(16).slice(1);
+
+
 }
 
-// when a rectangle is drawn, add it to the drawnItems feature group
-map.on('draw:created', function (e) {
-  var layer = e.layer;
-  drawnItems.addLayer(layer);
-  drawControl.remove();
-  drawControl.addTo(map);
+function OnChange() {
+  try {
+    let lat = parseFloat(document.getElementById("latitude").value);
+    let lon = parseFloat(document.getElementById("longitude").value);
+    let buf = parseFloat(document.getElementById("buffer").value);
+    let todate = td.value;
+    let fromdate = fd.value;
+    let calc = document.getElementById("calc");
+    let index = calc.value;
+    if (lat != NaN && lon != NaN && buf != NaN) {
+      let polygonCoordinates = [
+        [lat - buf, lon - buf],
+        [lat + buf, lon - buf],
+        [lat + buf, lon + buf],
+        [lat - buf, lon + buf]
+      ];
+      let lat_min = lat - buf;
+      let lat_max = lat + buf;
+      let lng_min = lon - buf;
+      let lng_max = lon + buf;
+      // Create a polygon using the coordinates
+      let col = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var polygon = L.polygon(polygonCoordinates, { color: col }).addTo(map);
+      let data = {
+        lat_min: lat_min,
+        lat_max: lat_max,
+        lng_min: lng_min,
+        lng_max: lng_max,
+        todate: todate,
+        fromdate: fromdate,
+        index: index
+      }
+      console.log(data)
+      // setTimeout(() => {
+      //   appendData('2023-06-01', 4, 'blue');
+      // }, 1000);
 
-    // get the coordinates of the selected area
-    let coordinates = layer.getLatLngs();
-    console.log(coordinates)
-    let lat_min = coordinates[0][0]["lat"];
-    let lat_max = coordinates[0][1]["lat"];
-    let lng_min = coordinates[0][0]["lng"];
-    let lng_max = coordinates[0][2]["lng"];
-    let data = {
-      lat_min: lat_min,
-      lat_max: lat_max,
-      lng_min: lng_min,
-      lng_max: lng_max
-    }
-    // console.log(data)
-    // $(document).ready(function (){
+      // setTimeout(() => {
+      //   appendData('2023-06-02', 6, 'green');
+      // }, 2000);
+
+      // setTimeout(() => {
+      //   appendData('2023-06-03', 2, 'red');
+      // }, 3000);
+      // console.log("Draw Color", )
       const xhr = new XMLHttpRequest();
       xhr.open("POST", "/my_flask_route", true);
       xhr.setRequestHeader("Content-Type", "application/json");
@@ -94,22 +305,28 @@ map.on('draw:created', function (e) {
       //   console.log(e)
       //   console.log("In Progress")
       // }
-      document.getElementById("loader").classList.remove("d-none")
+      document.getElementById("loader").classList.remove("d-none");
+      // document.getElementById("loader").scrollIntoView({ behavior: "smooth", block: "end" });
+      window.scrollTo(0, document.body.scrollHeight);
       xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.UNSENT) {
           console.log("In Progress");
         } else if (xhr.readyState === XMLHttpRequest.DONE) {
           if (xhr.status === 200) {
             // Process the response here
-            if(xhr.response.error){
+            if (xhr.response.error) {
               document.getElementById("loader").classList.add("d-none")
               document.getElementById('imgcont').innerHTML = xhr.response.status;
             }
-            else{
+            else {
               document.getElementById("loader").classList.add("d-none")
               const img = document.createElement('img');
               img.src = 'data:image/png;base64,' + xhr.response.image;
-        
+              img.style.width = "100%";
+              img.style.border = `2px solid ${col}`;
+              img.style.margin = "2rem 0rem";
+              img.style.borderRadius = "10px"
+
               // Add the image element to the document body
               document.getElementById('imgcont').appendChild(img);
             }
@@ -122,20 +339,184 @@ map.on('draw:created', function (e) {
       //     // Create an image element and set its source to the base64 encoded PNG image
       //     const img = document.createElement('img');
       //     img.src = 'data:image/png;base64,' + xhr.response.image;
-    
+
       //     // Add the image element to the document body
       //     document.getElementById('imgcont').appendChild(img);
-    
+
       //     // Hide the progress bar
       //     // $("#progress-bar").hide(1000);
       //   }
       // };
       xhr.send(JSON.stringify(data));
 
-    // })
-    // document.getElementById("my-form").submit();
+      // })
+      // document.getElementById("my-form").submit();
 
-    document.getElementById("lat_lon").innerHTML = `The Selected values range is <br>Latitude = (${lat_min}, ${lat_max})<br>Longitude = (${lng_min}, ${lng_max})`
+      document.getElementById("lat_lon").innerHTML = `The Selected values range is <br>Latitude = (${lat_min}, ${lat_max})<br>Longitude = (${lng_min}, ${lng_max})`
+    }
+  } catch (error) { }
+}
+
+// function handleOnChange(e){
+//   console.log(e.target.value)
+// }
+
+// map.on('layeradd', function(e) {
+//   var layer = e.layer;
+//   // Perform actions on the newly added polygon layer
+//   // let coordinates = layer.getLatLngs();
+//   // console.log(coordinates)
+//   console.log('New polygetLatLngs());gon layer added:',layer.
+// })
+var count = 0
+// when a rectangle is drawn, add it to the drawnItems feature group
+map.on('draw:created', function (e) {
+  var layer = e.layer;
+  layer.options.color = getRandomColor();
+  drawnItems.addLayer(layer);
+  drawControl.remove();
+  drawControl.addTo(map);
+
+  // get the coordinates of the selected area
+  let coordinates = layer.getLatLngs();
+  console.log(coordinates)
+  let lat_min = coordinates[0][0]["lat"];
+  let lat_max = coordinates[0][1]["lat"];
+  let lng_min = coordinates[0][0]["lng"];
+  let lng_max = coordinates[0][2]["lng"];
+  let todate = td.value;
+  let fromdate = fd.value;
+  let calc = document.getElementById("calc");
+  let index = calc.value;
+  let data = {
+    lat_min: lat_min,
+    lat_max: lat_max,
+    lng_min: lng_min,
+    lng_max: lng_max,
+    todate: todate,
+    fromdate: fromdate,
+    index: index
+  }
+  console.log(data)
+  // $(document).ready(function (){
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/my_flask_route", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.responseType = "json";
+
+  // xhr.onprogress = function (e){
+  //   // $("#progress-bar").html("In Progress")
+  //   console.log(e)
+  //   console.log("In Progress")
+  // }
+  document.getElementById("loader").classList.remove("d-none");
+  // document.getElementById("loader").scrollIntoView({ behavior: "smooth", block: "end" });
+  window.scrollTo(0, document.body.scrollHeight);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.UNSENT) {
+      console.log("In Progress");
+    } else if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // Process the response here
+        if (xhr.response.error) {
+          console.log(xhr.response.error);
+          document.getElementById("loader").classList.add("d-none");
+          const div = document.createElement('div');
+          const span = document.createElement('span');
+          let text = document.createElement('p');
+          div.style.width = "48%";
+          text.innerHTML = xhr.response.error;
+          text.style.width = "100%";
+          text.style.height = "80%";
+          text.style.border = `2px solid ${layer.options.color}`;
+          text.style.margin = "2rem 0rem";
+          text.style.display = "block"
+          text.style.borderRadius = "10px"
+          span.classList.add("badge", "text-bg-danger");
+          span.innerHTML = `${index}`;
+          span.style.float = "right";
+          span.style.margin = "1.2rem 0rem";
+          div.appendChild(span);
+          div.appendChild(text);
+          document.getElementById("imgcont").appendChild(div)
+        }
+        else {
+          document.getElementById("loader").classList.add("d-none")
+          // const maindiv = document.getElementById("mgr");
+          const div = document.createElement('div');
+          const span = document.createElement('span');
+          const img = document.createElement('img');
+          const m = document.getElementById('map');
+          // const h = document.getElementsByClassName('heading')[0];
+          // h.classList.remove()
+          // maindiv.classList.add("row")
+          // m.classList.add("col")
+          m.style.width = "50%"
+          performTask(xhr.response.labels);
+          let tableBody = document.getElementById('tbody');
+          let newRow = document.createElement('tr');
+          count++
+          // Create the HTML content for the new row
+          let rowContent = `<th style="background-color: ${layer.options.color}">${count}</th>`;
+          
+          for(i of xhr.response.data){
+            rowContent += `<td>${i}</td>`
+          }
+
+          // Set the HTML content of the new row
+          newRow.innerHTML = rowContent;
+
+          // Append the new row to the table body
+          tableBody.appendChild(newRow);
+          appendData({
+            label: '',
+            data: xhr.response.data,
+            fill: false,
+            borderColor: `${layer.options.color}`,
+            tension: 0.1
+          })
+          div.style.width = "48%";
+          img.src = 'data:image/png;base64,' + xhr.response.image;
+          img.style.width = "100%";
+          img.style.border = `2px solid ${layer.options.color}`;
+          img.style.margin = "2rem 0rem";
+          img.style.borderRadius = "10px"
+          span.classList.add("badge", "text-bg-primary");
+          span.innerHTML = `${xhr.response.area},${index}`;
+          span.style.float = "right";
+          span.style.margin = "1.2rem 0rem";
+          // updateGraph();
+          // div.style.margin = "0px 4px"
+          // document.getElementById('imgcont').appendChild("<span class='badge text-bg-primary'>Primary</span>")
+
+          // Add the image element to the document body
+          div.appendChild(span);
+          div.appendChild(img);
+          document.getElementById("imgcont").appendChild(div)
+        }
+      }
+    }
+  };
+  // xhr.addEventListener("load", function(event) {
+  // xhr.onload = function (){
+  //   if (xhr.status === 200) {
+  //     // Create an image element and set its source to the base64 encoded PNG image
+  //     const img = document.createElement('img');
+  //     img.src = 'data:image/png;base64,' + xhr.response.image;
+
+  //     // Add the image element to the document body
+  //     document.getElementById('imgcont').appendChild(img);
+
+  //     // Hide the progress bar
+  //     // $("#progress-bar").hide(1000);
+  //   }
+  // };
+  xhr.send(JSON.stringify(data));
+
+  // })
+  // document.getElementById("my-form").submit();
+
+  // document.getElementById("lat_lon").innerHTML = `The Selected values range is <br>Latitude = (${lat_min}, ${lat_max})<br>Longitude = (${lng_min}, ${lng_max})`
 });
 
 
@@ -179,3 +560,7 @@ map.on('draw:created', function (e) {
 //   }),
 //   target: 'map',
 // });
+
+
+// let fd = document.getElementsByName("fromdate");
+
